bb571ab57852d86c59989c4a4ec8de1c23d669f9
sarah wang <sarahwang47@gmail.com>
2020-08-22 12:38:21 +0800

    add delete for micropost

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -19,6 +19,15 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to login_url
   end
+
+  test "should redirect destroy for wrong micropost" do
+    log_in_as(users(:michael))
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(micropost)
+    end
+    assert_redirected_to root_url
+  end
   # test "the truth" do
   #   assert true
   # end
