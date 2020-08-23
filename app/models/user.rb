f6196b987aa65c2f89da1542cb1048f7b192a2ec
sarah wang <sarahwang47@gmail.com>
2020-08-23 17:50:10 +0800

    done follow and unfollow friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
+
   has_many :microposts
   has_many :microposts, dependent: :destroy
+  #we explicit says that our foreign_key is follower_id (ex: has_many user_posts, foreign_key: user_id)
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  #find the people's i'm following through active_relationships, followed_id. (ex has_many: posts, thought: user_posts)
+  has_many :following, through: :active_relationships, source: :followed #here we write followed to find the followed_id in the join table
+  #the other side of the has_many
+  has_many :passive_relationships, class_name: "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token,:activation_token,:reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -15,6 +27,19 @@ class User < ApplicationRecord
 
   def feed
     microposts
+  end
+
+  # 关注另一个用户
+  def follow(other_user)
+    following << other_user
+  end
+  # 取消关注另一个用户
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+  # 如果当前用户关注了指定的用户，返回 true
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   def self.digest(string)
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   get 'password_resets/new'
   get 'password_resets/edit'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
   get 'users/new'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   root 'static_pages#home'
   get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
@@ -15,5 +23,6 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
